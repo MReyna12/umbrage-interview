d6d3a53a2e6f4d91f107861b820640260e14925f
Michael <mreyna12@live.com>
2022-09-08 12:41:53 -0500

    add comments to auth and App

--- umbrage-interview/hooks/auth.jsx
+++ umbrage-interview/hooks/auth.jsx
@@ -1,11 +1,15 @@
 import { useContext, createContext } from "react";
 import { useNavigate } from "react-router-dom";
 
+// createContext allows the passing down and use of data in any component within the React app without using props
+// Helps to reduce props driling, which is passing props down multiple level of nested components that don't need the props
 const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
   const navigate = useNavigate();
 
+  // This function runs when the user submits their username/password; however, if the incorrect username/password are entered, no bearer token is generated and the user must try again.
+  // If the correct username/password are entered, then the bearer token returned as a response from the fetch is put in localStorage and the user is then routed to /avatars (which displays the Avatars component).
   const login = async (user) => {
     const loginUrl = "https://umbrage-interview-api.herokuapp.com/login";
     const settings = {
@@ -32,12 +36,14 @@ export const AuthProvider = ({ children }) => {
     }
   };
 
-  // When a user logouts localstorage is cleared and the user is sent back to the login page
+  // When a user logouts localstorage is cleared and the user is sent back to the login page.
+  // Setting the property "replace" to true does not allow the user to go back to a previously protected route by clicking the back button
   const logout = () => {
     localStorage.clear();
     navigate("/", { replace: true });
   };
 
+  // In App.jsx, AuthProvider will wrap all of the components (the children value in the return value below), thus allowing for global access of login/logout
   return (
     <AuthContext.Provider value={{ login, logout }}>
       {children}
@@ -45,6 +51,7 @@ export const AuthProvider = ({ children }) => {
   );
 };
 
+// Whenever useAuth is called throughout the code base, the values(login, logout) from the context object (AuthContext) will be accessible
 export const useAuth = () => {
   return useContext(AuthContext);
 };
--- umbrage-interview/src/App.jsx
+++ umbrage-interview/src/App.jsx
@@ -5,6 +5,8 @@ import Login from "../components/Login";
 import ProtectedRoute from "../components/ProtectedRoute";
 import { AuthProvider } from "../hooks/auth";
 
+// AuthProvider allows for the data passed via context to be accessible to all of its children
+// ProtectedRoute is a component that checks to see if the user has been authenticated (received a bearer token), if not, the avatars and detailedview routes will be inaccessible
 function App() {
   return (
     <AuthProvider>
